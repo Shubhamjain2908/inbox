0886ac8023105c7c221713ea404ab95b9fc9f311
Shubham Jain <shubham.jain@engineerbabu.in>
2020-02-13 00:56:35 +0530

    added solidity contracts

---
+++ contracts/Inbox.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.4.17;
+
+contract Inbox {
+    string public message;
+
+    // constructor function (fnc name === contract name)
+    // function Inbox(string initialMessage) public {
+    //     message = initialMessage;
+    // }
+
+    constructor(string initialMessage) public {
+        message = initialMessage;
+    }
+
+    function setMessage(string newMessage) public {
+        message = newMessage;
+    }
+
+    // function getMessage() public view returns(string) {
+    //     return message;
+    // }
+
+}
